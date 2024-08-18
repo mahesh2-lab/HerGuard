34108e94a3452a757f8bf7de7a52ec372bf0414b
mahesh2-lab <maheshchopade133@gmail.com>
2024-08-18 23:06:14 +0530

    a

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import React , {useEffect ,useState} from "react";
+import { MapContainer, TileLayer , Marker, Popup, useMap } from "react-leaflet";
 import { HeatmapLayer } from "react-leaflet-heatmap-layer-v3"; // Ensure correct import path
 import "leaflet/dist/leaflet.css";
 import FloatingNavBar from "./FloatingNavBar"; 
 import { Detail } from "./detail";
+import L from "leaflet";
+
+import icon from "./icon";
 
 const center = [20.536846, 76.180870];
+
 const points = [
   { lat: 20.536846, lng: 76.180870, intensity: 0.5 },
   { lat: 20.546846, lng: 76.190870, intensity: 0.8 },
@@ -30,7 +34,45 @@ const heatmapData = {
   },
 };
 
-const Map = () => (
+
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+  const [bbox, setBbox] = useState([]);
+
+  const map = useMap();
+
+  useEffect(() => {
+    map.locate().on("locationfound", function (e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+      const radius = e.accuracy;
+      const circle = L.circle(e.latlng, radius);
+      circle.addTo(map);
+      setBbox(e.bounds.toBBoxString().split(","));
+    });
+  }, [map]);
+
+  return position === null ? null : (
+    <Marker position={position} icon={icon}>
+      <Popup>
+        You are here. <br />
+        Map bbox: <br />
+        <b>Southwest lng</b>: {bbox[0]} <br />
+        <b>Southwest lat</b>: {bbox[1]} <br />
+        <b>Northeast lng</b>: {bbox[2]} <br />
+        <b>Northeast lat</b>: {bbox[3]}
+      </Popup>
+    </Marker>
+  );
+}
+
+const Map = () => {
+
+
+
+
+return (
   <div className="relative w-full h-screen">
     <FloatingNavBar />
     <MapContainer center={center} zoom={13} className="w-full h-full"  zoomControl={true}>
@@ -47,10 +89,12 @@ const Map = () => (
         maxZoom={heatmapData.options.maxZoom}
         gradient={heatmapData.options.gradient}
       />
+      <LocationMarker />
 
     </MapContainer>
     <Detail/>
   </div>
 );
+};
 
 export default Map;
